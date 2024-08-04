2e45147c8e5dd87dbedebc494ac9b9a504ea6431
Muhammed Ozmen <m.ozmenn34@gmail.com>
2024-08-04 18:04:43 +0300

    v1.0.0

--- game.py
+++ game.py
@@ -26,7 +26,7 @@ BLUE2 = (0, 100, 255)
 BLACK = (0, 0, 0)
 
 BLOCK_SIZE = 20
-SPEED = 20
+SPEED = 100
 
 
 class SnakeGameAI:
@@ -99,7 +99,7 @@ class SnakeGameAI:
         if pt is None:
             pt = self.head
         # hits boundary
-        if pt.head.x > self.w - BLOCK_SIZE or pt.x < 0 or pt.y > self.h - BLOCK_SIZE or pt.y < 0:
+        if pt.x > self.w - BLOCK_SIZE or pt.x < 0 or pt.y > self.h - BLOCK_SIZE or pt.y < 0:
             return True
         # hits itself
         if pt in self.snake[1:]:
